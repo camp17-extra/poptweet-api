bc6afe3a2d0894b7fbfa7b6ece4484a9e77a4251
bake0937 <sadayasu.info@gmail.com>
2018-04-12 08:26:07 +0900

    module定義を削除

--- app/controllers/api/v1/tweets_controller.rb
+++ app/controllers/api/v1/tweets_controller.rb
@@ -1,38 +1,34 @@
-module Api
-  module V1
-    class Api::V1::TweetsController < ApplicationController
-      def index
-        @tweets = Tweet.all
-        render json: @tweets
-      end
-
-      def create
-        @tweet = Tweet.create(tweet_params)
-        render json: @tweet
-      end
-
-      def destroy
-        tweet = Tweet.find(params[:id])
-        tweet.destroy
-        render json: tweet
-      end
-
-      def update
-        tweet = Tweet.find(params[:id])
-        tweet.update(tweet_params)
-        render json: tweet
-      end
-
-      def show
-        tweet = Tweet.find(params[:id])
-        render json: tweet
-      end
-
-      private
-      def tweet_params
-        params.permit(:name, :image, :text)
-      end
-
-    end
+class Api::V1::TweetsController < ApplicationController
+  def index
+    @tweets = Tweet.all
+    render json: @tweets
   end
+
+  def create
+    @tweet = Tweet.create(tweet_params)
+    render json: @tweet
+  end
+
+  def destroy
+    tweet = Tweet.find(params[:id])
+    tweet.destroy
+    render json: tweet
+  end
+
+  def update
+    tweet = Tweet.find(params[:id])
+    tweet.update(tweet_params)
+    render json: tweet
+  end
+
+  def show
+    tweet = Tweet.find(params[:id])
+    render json: tweet
+  end
+
+  private
+  def tweet_params
+    params.permit(:name, :image, :text)
+  end
+
 end
