45d40967d08595ae6ebe28cfa5627f5b4a9f18d0
popmac <parade8823@gmail.com>
2018-04-28 22:00:57 +0900

    シードを作成するコードをメソッドにした
    後で使い回せるようにするため

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,11 @@
-user = User.create(email: 'user1@example.com', password: 'mypass')
+def create_user(email:, password:)
+  User.create(email: email, password: password)
+end
 
-Tweet.create(name: 'タイトル', text: 'テキスト', image: '画像のURL', user: user)
+def create_tweet(name:, text:, image:, user:)
+  Tweet.create(name: name, text: text, image: image, user: user)
+end
+
+user = create_user(email: 'user1@example.com', password: 'mypass')
+
+create_tweet(name: 'タイトル', text: 'テキスト', image: '画像のURL', user: user)
