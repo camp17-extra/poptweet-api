a073f7145669cf66fd62e6cca2c216f1def7d509
popmac <parade8823@gmail.com>
2018-04-28 18:24:00 +0900

    Replyモデルを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180424135408) do
+ActiveRecord::Schema.define(version: 20180428091603) do
+
+  create_table "replies", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.bigint "user_id", null: false
+    t.bigint "tweet_id", null: false
+    t.text "text", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["tweet_id"], name: "index_replies_on_tweet_id"
+    t.index ["user_id"], name: "index_replies_on_user_id"
+  end
 
   create_table "tweets", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "name"
