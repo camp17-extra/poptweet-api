ed2a993148ffee294c35f3458c177fdb3ed37b74
popmac <parade8823@gmail.com>
2018-04-28 18:50:07 +0900

    返信のルーティングを追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   devise_for :user, only: []
   namespace :api, {format: 'json'} do
     namespace :v1 do
-      resources :tweets
+      resources :tweets do
+        resources :replies, only: [:create, :update, :destroy]
+      end
       resource :login, only: [:create], controller: :sessions
       resources :users, only: [:show, :create]
     end
