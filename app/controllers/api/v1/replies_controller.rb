17678229d8632aa7a28c371ba141c703419854be
popmac <parade8823@gmail.com>
2018-04-28 19:50:14 +0900

    返信を作成するアクションを追加

--- app/controllers/api/v1/replies_controller.rb
+++ app/controllers/api/v1/replies_controller.rb
@@ -1,2 +1,13 @@
 class Api::V1::RepliesController < ApplicationController
+  def create
+    tweet = Tweet.find(params[:tweet_id])
+    reply = tweet.replies.build(reply_params)
+    reply.save
+    render json: reply
+  end
+
+  private
+  def reply_params
+    params.require(:reply).permit(:text).merge(user_id: current_user.id)
+  end
 end
