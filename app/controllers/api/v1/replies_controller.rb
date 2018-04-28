feec8c8ffed0a72dee868ea15f6cc44328fc21a6
popmac <parade8823@gmail.com>
2018-04-28 19:59:35 +0900

    返信を更新するアクションを追加

--- app/controllers/api/v1/replies_controller.rb
+++ app/controllers/api/v1/replies_controller.rb
@@ -6,6 +6,14 @@ class Api::V1::RepliesController < ApplicationController
     render json: reply
   end
 
+  def update
+    tweet = Tweet.find(params[:tweet_id])
+    reply = tweet.replies.find(params[:id])
+    reply.assign_attributes(reply_params)
+    reply.save
+    render json: reply
+  end
+
   private
   def reply_params
     params.require(:reply).permit(:text).merge(user_id: current_user.id)
