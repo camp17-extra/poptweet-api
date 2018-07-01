46de7a50b1c817c922b8e41d3d86a89c58167d33
bake0937 <sadayasu.info@gmail.com>
2018-07-01 22:04:56 +0900

    環境変数でAllowed Originを指定できるようにする

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,7 @@ module Poptweet
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
+    config.x.cors_allowed_origins = ENV.fetch('CORS_ALLOWED_ORIGINS', 'localhost:3000')
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
